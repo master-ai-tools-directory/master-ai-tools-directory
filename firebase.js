715a08eabc3d2ed4b274bcc6ff0eee63b40d291c
master-ai-tools-directory <earnmoneywithrashid@gmail.com>
2026-07-03 17:12:00 +0500

    Refactor firebaseConfig export syntax

--- firebase.js
+++ firebase.js
@@ -1,10 +1,8 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-const firebaseConfig = {
-  apiKey:  "AIzaSyCL4AaIpTwhdehdAg83ZBNO0QxaFVZhhWQ",
-authDomain: "ai-tools-website-7d277.firebaseapp.com",
-projectId: "ai-tools-website-7d277",
-storageBucket: "ai-tools-website-7d277.firebasestorage.app",
-messagingSenderId: "351773858776",
-appId: "1:351773858776:web:6fc7a597bbcee3200930ad"
+export const firebaseConfig = {
+  apiKey: "AIzaSyCL4AaIpTwhdehdAg83ZBNO0QxaFVZhhWQ",
+  authDomain: "ai-tools-website-7d277.firebaseapp.com",
+  projectId: "ai-tools-website-7d277",
+  storageBucket: "ai-tools-website-7d277.firebasestorage.app",
+  messagingSenderId: "351773858776",
+  appId: "1:351773858776:web:6fc7a597bbcee3200930ad"
+};
