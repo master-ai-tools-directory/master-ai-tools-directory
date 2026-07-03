bfe91ca73f00dda1e25d935fcd9883fdbb459424
master-ai-tools-directory <earnmoneywithrashid@gmail.com>
2026-07-03 12:59:03 +0500

    Add Firebase configuration for Firestore
    Added Firebase configuration object for Firestore setup.

--- firebase.js
+++ firebase.js
@@ -1,3 +1,11 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
