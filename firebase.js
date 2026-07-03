4f935399a54437ef7997c948ec27a53daaa3f22d
master-ai-tools-directory <earnmoneywithrashid@gmail.com>
2026-07-03 13:18:32 +0500

    Firebase Config Updated
    Updated Firebase configuration with actual credentials.

--- firebase.js
+++ firebase.js
@@ -2,10 +2,9 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/fireba
 
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
-};
+  apiKey:  "AIzaSyCL4AaIpTwhdehdAg83ZBNO0QxaFVZhhWQ",
+authDomain: "ai-tools-website-7d277.firebaseapp.com",
+projectId: "ai-tools-website-7d277",
+storageBucket: "ai-tools-website-7d277.firebasestorage.app",
+messagingSenderId: "351773858776",
+appId: "1:351773858776:web:6fc7a597bbcee3200930ad"
