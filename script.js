b8e234b39bd543b8294490be420bc3cdd676c230
master-ai-tools-directory <earnmoneywithrashid@gmail.com>
2026-07-03 13:24:36 +0500

    Integrate Firebase into script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("AI Tools Directory Started");
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+import { firebaseConfig } from "./firebase.js";
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+window.db = db;
+
+console.log("Firebase Connected Successfully");
