bd23cd571a384698b9c1476e96932e6c0fecf2d8
master-ai-tools-directory <earnmoneywithrashid@gmail.com>
2026-07-03 12:57:51 +0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,3 @@
-// Firebase Config
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
