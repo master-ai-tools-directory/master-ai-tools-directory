0f3c4a99d4b95e46502878a718c01659f775874a
master-ai-tools-directory <earnmoneywithrashid@gmail.com>
2026-07-05 10:55:34 +0500

    Update Firebase API key and messaging sender ID

--- firebase.js
+++ firebase.js
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyCL4AaIpTwhdehdAg83ZBNO0QxaFVZhhWQ",
+  apiKey: "AIzaSyCa4aJafYndhehdAg83ZBNO0QxafVZhnWQ",
   authDomain: "ai-tools-website-7d277.firebaseapp.com",
   projectId: "ai-tools-website-7d277",
   storageBucket: "ai-tools-website-7d277.firebasestorage.app",
-  messagingSenderId: "351773858776",
-  appId: "1:351773858776:web:6fc7a597bbcee3200930ad"
+  messagingSenderId: "351773585776",
+  appId: "1:351773585776:web:6fc7a597bbcee3200930ad"
 };
